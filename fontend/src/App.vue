21f698d1658c09820fb9eb336e7b14c80df460e4
63070028 <63070028@kmitl.ac.th>
2023-01-26 23:18:42 +0700

    add SignUp page

---
+++ fontend/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+   <SignUp/>
+</template>
+
+<script>
+import SignUp from './components/SignUp.vue';
+export default {
+  name: 'App',
+  components:{
+    SignUp
+  },
+  data(){
+    return{
+    }
+  }
+
+}
+</script>
+
+<style >
+#app {
+  background-color: #eeeeee;
+  padding: 3em;
+  height: 100%;
+}
+</style>
---
+++ fontend/src/components/SignUp.vue
@@ -0,0 +1,96 @@
+<template>
+    <div class="signup">
+
+        <h1 class="is-size-1 has-text-centered">Sign Up</h1>
+
+        <div class="field">
+            <label class="label is-size-4">First Name</label>
+            <div class="control">
+                <input class="input" type="text" v-model="firstName">
+            </div>
+        </div>
+
+        <div class="field">
+            <label class="label is-size-4">Last Name</label>
+            <div class="control">
+                <input class="input" type="text" v-model="lastName">
+            </div>
+        </div>
+
+        <div class="field">
+            <label class="label is-size-4">Email</label>
+            <div class="control">
+                <input class="input" type="email" v-model="email">
+            </div>
+        </div>
+
+        <div class="field">
+            <label class="label is-size-4">Password</label>
+            <div class="control">
+                <input class="input" type="password" v-model="password">
+            </div>
+        </div>
+
+        <div class="field">
+            <label class="label is-size-4">Confirm Password</label>
+            <div class="control">
+                <input class="input" type="text" v-model="confirmPassword">
+            </div>
+        </div>
+
+        <div class="field">
+            <label class="label is-size-4">Gender</label>
+            <div class="control">
+                <div class="select">
+                    <select v-model="gender">
+                        <option value="">-- select --</option>
+                        <option value="M">Male</option>
+                        <option value="F">Female</option>
+                        <option value="U">Unknow</option>
+                    </select>
+                </div>
+            </div>
+        </div>
+
+        <div class="field">
+            <div class="control">
+                <button class="button is-success is-large is-fullwidth">Submit</button>
+            </div>
+        </div>
+
+    </div>
+</template>
+
+
+<script>
+export default {
+    name: "SignUp",
+    data(){
+        return{
+            firstName:"",
+            lastName:"",
+            email:"",
+            password:"",
+            confirmPassword:"",
+            gender:""
+        }
+    }
+
+
+}
+</script>
+
+<style scoped>
+.signup {
+    margin:auto;
+    padding: 3em;
+    width: 50%;
+    background-color: white;
+    /* border-style: solid;
+    border-width: 5px; */
+}
+
+.field {
+    margin-top: 2em;
+}
+</style>
